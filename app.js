70752014bddc061776660096fc0b3deeb6ce3684
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2022-02-04 15:30:08 +0000

    foo

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ let provider = ganache.provider({
 	fork: {
 		network: "mainnet",
 		// blockNumber: 14048830 + 1,
-		blockNumber: 14048830,
+		// blockNumber: 14048830,
+		// blockNumber: 14128780,
+		blockNumber: 14128830,
 	},
 	logging: {
 		quiet: true,
@@ -46,7 +48,9 @@ let contracts = [];
 
 console.log("loading solidity compiler...");
 
-solc.loadRemoteVersion("v0.7.4+commit.3f05b770", (error, solcSnapshot) => {
+// solc.loadRemoteVersion("v0.7.4+commit.3f05b770", (error, solcSnapshot) => {
+// solc.loadRemoteVersion("v0.4.15+commit.bbb8e64f", (error, solcSnapshot) => {
+solc.loadRemoteVersion("v0.6.6+commit.6c089d02", (error, solcSnapshot) => {
 	console.log("starting compilation process...");
 
 	if (error) {
@@ -54,12 +58,14 @@ solc.loadRemoteVersion("v0.7.4+commit.3f05b770", (error, solcSnapshot) => {
 		console.error(error);
 	}
 
-	let source = fs.readFileSync("./code.sol").toString();
+	// let source = fs.readFileSync("./code.sol").toString();
+	let source = fs.readFileSync("./code2.sol").toString();
 
 	let input = {
 		language: "Solidity",
 		sources: {
-			"code.sol": {
+			// "code.sol": {
+			"code2.sol": {
 				content: source,
 			},
 		},
@@ -91,8 +97,10 @@ solc.loadRemoteVersion("v0.7.4+commit.3f05b770", (error, solcSnapshot) => {
 			contracts.push({
 				contractName,
 				source,
-				sourcePath: "./code.sol",
-				ast: output.sources["code.sol"].ast,
+				// sourcePath: "./code.sol",
+				sourcePath: "./code2.sol",
+				// ast: output.sources["code.sol"].ast,
+				ast: output.sources["code2.sol"].ast,
 				binary: `0x${contractData.evm.bytecode.object}`,
 				sourceMap: contractData.evm.bytecode.sourceMap,
 				deployedBinary: `0x${contractData.evm.deployedBytecode.object}`,
@@ -113,7 +121,9 @@ console.log("waiting to be ready...");
 await promise;
 console.log("ready waiting!");
 
-let transactionHash = "0x03e784bfcb8f6175f7a321586f671dff08a87070f62f5c9ae928e8f2c31e17bd";
+// let transactionHash = "0x03e784bfcb8f6175f7a321586f671dff08a87070f62f5c9ae928e8f2c31e17bd";
+// let transactionHash = "0x5927ebe3c0fbb223ac099d10da6b57bada7add95c6ec564c5f5e58f892adfe86";
+let transactionHash = "0x5c4e25f92bc77d8132e49d28efa51eee8b4406d0dfa87130f2bfb362358ea106";
 
 console.log("intiailizing debugger...");
 
@@ -135,7 +145,7 @@ await session.ready();
 console.log("debugger session ready!");
 
 console.log("Stepping...");
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10000; i++) {
 	let result = await session.stepNext();
 	// console.log("Stepping result:", result);
 }
@@ -150,5 +160,8 @@ let { ast, data, evm, solidity, trace } = Debugger.selectors;
 let view = session.view(data.current.identifiers.sections);
 fs.writeFileSync("./view.json", JSON.stringify(view, null, 2));
 console.log(view);
+let variables = await session.variables();
+fs.writeFileSync("./variables.json", JSON.stringify(variables, null, 2));
+console.log(variables);
 
 console.log("DONE");
